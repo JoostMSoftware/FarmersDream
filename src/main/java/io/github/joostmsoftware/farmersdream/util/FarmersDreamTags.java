eb7ac698ea3121dc87a9d142ff946854b220137d
JoostMSoftware <joostmsoftware@gmail.com>
2022-09-13 14:16:09 +0200

    forgot to add the actual tags, whoops

--- src/main/java/io/github/joostmsoftware/farmersdream/util/FarmersDreamTags.java
+++ src/main/java/io/github/joostmsoftware/farmersdream/util/FarmersDreamTags.java
@@ -1,6 +1,7 @@
 package io.github.joostmsoftware.farmersdream.util;
 
 import net.minecraft.fluid.Fluid;
+import net.minecraft.item.Item;
 import net.minecraft.tag.TagKey;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -11,6 +12,10 @@ public class FarmersDreamTags {
 
     public static final TagKey<Fluid> BIO_FLUID = TagKey.of(Registry.FLUID_KEY, new Identifier(MOD_ID, "bio_fluid"));
     public static final TagKey<Fluid> COMMON_BIO_FLUID = TagKey.of(Registry.FLUID_KEY, new Identifier("c:bio_fluid"));
+
+    public static final TagKey<Item> COMMON_CORN_CROP = TagKey.of(Registry.ITEM_KEY, new Identifier("c:crops/corn"));
+    public static final TagKey<Item> COMMON_GRANOLA_CROP = TagKey.of(Registry.ITEM_KEY, new Identifier("c:crops/granola"));
+    public static final TagKey<Item> COMMON_PINEAPPLE_CROP = TagKey.of(Registry.ITEM_KEY, new Identifier("c:crops/pineapple"));
 
     public static void registerTags() {
 
